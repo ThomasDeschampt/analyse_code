e9d976f3a5b9c96adff7c12561d78607ac37b45c
Thomas Deschampt <thomas.deschampt@epsi.fr>
2025-06-17 14:25:35 +0200

    moins 10

--- app/src/main/java/com/simplecity/amp_library/glide/utils/Size.java
+++ app/src/main/java/com/simplecity/amp_library/glide/utils/Size.java
@@ -5,12 +5,10 @@ public class Size {
     public int width;
     public int height;
 
-    public Size(int width, int height) {
-        {
-            this.width = width;
-            this.height = height;
-        }
-    }
+public Size(int width, int height) {
+    this.width = width;
+    this.height = height;
+}
 
     @Override
     public boolean equals(Object o) {
